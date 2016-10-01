2396c20fadaf63ee07d46e196125a02626139517
fin <fin@localhost.localdomain>
2016-10-02 02:39:20 +0700

    keep it simple

--- app/components/App.js
+++ app/components/App.js
@@ -3,8 +3,7 @@ import React from 'react'
 export default class App extends React.Component {
 	render() {
 		return (
-			<div>React Express Mobx Starter Kit!!!
-			</div>
+			<div>React Express Mobx Starter Kit!!!</div>
 		)
 	}
 }
